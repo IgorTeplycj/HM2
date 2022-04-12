18e5630893a3d98355818a22a7bd459a8eed71c3
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-04-12 12:54:28 +0600

    Add new files

---
+++ HM2/GameSolve/Actions/Move.cs
@@ -0,0 +1,27 @@
+﻿using HM2.GameSolve.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.GameSolve.Actions
+{
+    public class Move
+    {
+        Movable _movable;
+        public Move(Movable movable)
+        {
+            _movable = movable;
+        }
+
+
+        public void Execute()
+        {
+            if (this._movable == null)
+                throw new Exception("Объект, реализующий интерфейс Movable, не определен.");
+
+            _movable.Set(this._movable.CurrentVector.Add(this._movable.CurrentVector));
+        }
+    }
+}
