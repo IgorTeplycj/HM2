2936170fe24b088423de6cd0e4782f706c3da6cc
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-04-12 13:00:13 +0600

    Добавил тесты

---
+++ HM2/GameSolve/Actions/Rotate.cs
@@ -0,0 +1,28 @@
+﻿using HM2.GameSolve.Interfaces;
+using HM2.GameSolve.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.GameSolve.Actions
+{
+    public class Rotate
+    {
+        Movable rot;
+        public Rotate(Movable _rot)
+        {
+            rot = _rot;
+        }
+
+
+        public void Execute()
+        {
+            Vector vector = new Vector();
+            vector = rot.CurrentVector;
+            vector.Direction = rot.CurrentVector.NextDirection;
+            rot.Set(vector);
+        }
+    }
+}
