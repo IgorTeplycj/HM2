3a7932d9e653e3c7e19e141e9ae2824ac53e04e8
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-04-23 18:01:48 +0600

    Добавил IoC контейнер

---
+++ HM2/IoCs/IoC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.IoCs
+{
+    public class IoC<T>
+    {
+        IDictionary<string, T> collection = new Dictionary<string, T>();
+
+        public void Resolve<T>()
+        {
+
+        }
+    }
+}
