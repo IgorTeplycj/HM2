36e3345ba5eba19d8a3dc5c22c519715bebbee49
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-04-26 22:03:12 +0600

    Переработал IoC. Добавил таблицу методов.

--- HM2/IoCs/IoC.cs
+++ HM2/IoCs/IoC.cs
@@ -10,17 +10,26 @@ namespace HM2.IoCs
     public class IoCException : Exception { }
     public static class IoC<T>
     {
+        delegate T MyFunc(params object[] args);
+
         static IDictionary<string, T> container = new Dictionary<string, T>();
+        static IDictionary<string, MyFunc> methods = new Dictionary<string, MyFunc>
+        {
+            ["Registration"] = Registration,
+            ["IoC.Registration"] = Registration
+        };
+
         public static T Resolve(string key, params object[] obj)
         {
-            if (key.Contains("Registration"))
-            {
-                return Add((string)obj[0], (T)obj[1]);
-            }
-            else
-            {
-                return Get(key);
-            }
+            if (methods.ContainsKey(key))
+                return methods[key].Invoke(obj);
+
+            return Get(key);
+        }
+
+        static T Registration(params object[] args)
+        {
+            return Add((string)args[0], (T)args[1]);
         }
 
         static T Add(string key, T val)
