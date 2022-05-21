5d8ff14aa7c223abdc361d50f4b60224acb02a49
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-05-21 17:45:44 +0600

    Добавил адаптер без генерации кода

---
+++ HM2/GameSolve/Interfaces/IMovable.cs
@@ -0,0 +1,10 @@
+﻿namespace HM2.GameSolve.Interfaces
+{
+    public interface IMovable
+    {
+        public HM2.GameSolve.Structures.Vector getPosition();
+        public HM2.GameSolve.Structures.Vector setPosition(HM2.GameSolve.Structures.Vector newValue);
+        public HM2.GameSolve.Structures.Vector getVelocity();
+    }
+}
+
---
+++ HM2/MovableObject/Uobject.cs
@@ -0,0 +1,23 @@
+﻿using HM2.GameSolve.Interfaces;
+using HM2.GameSolve.Structures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM2.MovableObject
+{
+    public class UObject : IAction
+    {
+        public UObject(Vector v)
+        {
+            CurrentVector = v;
+        }
+        public Vector CurrentVector { get; set; }
+        public void Set(Vector newV)
+        {
+            CurrentVector = newV;
+        }
+    }
+}
