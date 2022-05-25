507c5aad818462c0da726a9a66fae00cd18f26e5
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-05-25 12:45:11 +0600

    -

--- HM2.Queue.Tests/TestsQueue.cs
+++ HM2.Queue.Tests/TestsQueue.cs
@@ -74,7 +74,7 @@ namespace HM2.Queue.Tests
             //отправка команды в очередь
             queueCommand.PushCommand(commandStart);
 
-            Thread.Sleep(10);
+            Thread.Sleep(50);
 
             void StartThread()
             {
