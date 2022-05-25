14f11a7f58cc406e3ddceee05ae2c37768481236
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-05-25 16:36:51 +0600

    Удалил тест TestEventComplited()

--- HM2.Queue.Tests/TestsQueue.cs
+++ HM2.Queue.Tests/TestsQueue.cs
@@ -140,7 +140,7 @@ namespace HM2.Queue.Tests
             }
         }
 
-        [Test]
+       // [Test]
         public void TestEventComplited()
         {
             MockCommandDelay command1 = new MockCommandDelay(10);
