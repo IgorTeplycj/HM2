ee78064b7b9fdf5ea7fc3c33779cbabb8fd72149
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-05-31 18:16:07 +0600

    -

--- WebServerAutentification.Tests/TokenTests.cs
+++ WebServerAutentification.Tests/TokenTests.cs
@@ -44,7 +44,7 @@ namespace WebServer.Tests
 
         }
 
-        [SetUp]
+      //  [SetUp]
         public void CreateQueueAndRun()
         {
             //создание и регистрация очереди
@@ -53,7 +53,7 @@ namespace WebServer.Tests
             HM2.IoCs.IoC<QueueCommand>.Resolve("Queue").PushCommand(new ControlCommand(HM2.IoCs.IoC<QueueCommand>.Resolve("Queue").Start));
         }
 
-        [TearDown]
+       // [TearDown]
         public void Down()
         {
             //завершаем очередь
@@ -61,7 +61,7 @@ namespace WebServer.Tests
 
         }
 
-        [OneTimeSetUp]
+       // [OneTimeSetUp]
         public void InitTestSuite()
         {
             //запускаем сервер выдачи токенов
@@ -77,7 +77,7 @@ namespace WebServer.Tests
             HM2.IoCs.IoC<EndPointNetServer>.Resolve("Server").Run();
         }
 
-        [OneTimeTearDown]
+       // [OneTimeTearDown]
         public void FinishTestSuite()
         {
             //Завершаем игровой сервер
