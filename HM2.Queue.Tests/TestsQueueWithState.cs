c759ca3997b12b40d34e80dd9742008228271a8f
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-06-05 14:29:18 +0600

    -

--- HM2.Queue.Tests/TestsQueueWithState.cs
+++ HM2.Queue.Tests/TestsQueueWithState.cs
@@ -104,7 +104,7 @@ namespace HM2.Queue.Tests
             QueueThread reservedQueue = IoC<QueueThread>.Resolve("ReservedQueue"); //получаем резервную очередь из контейнера
             reservedQueue.PushCommand(new ControlCommand(reservedQueue.Start)); //Запусскаем резервную очередь
 
-            Thread.Sleep(100);
+            Thread.Sleep(1000);
             Assert.IsTrue(command4.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
                                                           //   Assert.IsTrue(command5.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
                                                           // Assert.IsTrue(command6.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
