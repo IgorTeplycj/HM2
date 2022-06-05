483b2e067fa10cb7fff8eceda900631d20467de3
IgorTeplych <91873250+IgorTeplych@users.noreply.github.com>
2022-06-05 14:09:26 +0600

    Увеличил время ожидания выполнения асинхронных команд в тесте TestsQueueWithState.TestMoveToCommand()

--- HM2.Queue.Tests/TestsQueueWithState.cs
+++ HM2.Queue.Tests/TestsQueueWithState.cs
@@ -102,7 +102,7 @@ namespace HM2.Queue.Tests
             QueueThread reservedQueue = IoC<QueueThread>.Resolve("ReservedQueue"); //получаем резервную очередь из контейнера
             reservedQueue.PushCommand(new ControlCommand(reservedQueue.Start)); //Запусскаем резервную очередь
 
-            Thread.Sleep(40);
+            Thread.Sleep(100);
             Assert.IsTrue(command4.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
             Assert.IsTrue(command5.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
             Assert.IsTrue(command6.CommandIsComplited()); //Проверяем выполнение перегруженных команд в резервной очереди
